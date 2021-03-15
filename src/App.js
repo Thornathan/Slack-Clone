24a00b56f020540c2c379f6701f39ff2c7955ff8
Jonathan Lane <JlaneWebDev@gmail.com>
2021-03-15 15:27:54 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route path="/room">Chat Page</Route>
+          <Route path="/">Login Page</Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
